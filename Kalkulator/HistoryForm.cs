72562ae5aff303b8df0b3a9990871760d0e645cc
stijepic00 <stijepic404@gmail.com>
2026-04-24 13:23:51 +0200

    Add files via upload

--- Kalkulator/HistoryForm.cs
+++ Kalkulator/HistoryForm.cs
@@ -1,24 +1,34 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Kalkulator
 {
     public partial class HistoryForm : Form
     {
+        // Boje usklađene sa tvojim Dark Mode-om iz Form1
+        private Color darkBackground = Color.FromArgb(45, 45, 48);
+        private Color cardColor = Color.FromArgb(30, 30, 30);
+        private Color textColor = Color.White;
+        private Color resultColor = Color.Lime;
+
         public HistoryForm()
         {
             InitializeComponent();
+            // Postavljamo osnovni izgled forme
+            this.BackColor = darkBackground;
+            this.Text = "Istorija Računanja";
         }
 
         private void HistoryForm_Load(object sender, EventArgs e)
         {
-            // Automatski učitaj poslednju operaciju kada se forma otvori
-            UcitajPoslednjuOperaciju();
+            // Pozivamo učitavanje kartica
+            UcitajIstorijuUKartice();
         }
 
-        private void UcitajPoslednjuOperaciju()
+        private void UcitajIstorijuUKartice()
         {
             string connectionString = "Server=DESKTOP-J73S4GL;Database=KalkulatorDB;Trusted_Connection=True;";
 
@@ -27,31 +37,84 @@ namespace Kalkulator
                 using (SqlConnection conn = new SqlConnection(connectionString))
                 {
                     conn.Open();
-
-                    // Uzima sve operacije, poslednja operacija je na vrhu
                     string query = "SELECT * FROM Istorija ORDER BY Datum DESC";
 
                     SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
                     DataTable table = new DataTable();
                     adapter.Fill(table);
 
-                    dataGridView1.DataSource = table;
+                    // DODAJ OVU LINIJU ZA TEST:
+                    MessageBox.Show("Broj redova u bazi: " + table.Rows.Count);
 
-                    // Prilagodi širinu kolona
-                    dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                    // 1. Očisti flowHistory od starih stvari ili DataGridView-a (ako je ostao u kodu)
+                    flowHistory.Controls.Clear();
+                    flowHistory.FlowDirection = FlowDirection.TopDown;
+                    flowHistory.WrapContents = false;
+                    flowHistory.AutoScroll = true;
 
-                    // Opcionalno: označi poslednji red (poslednju operaciju)
-                    if (dataGridView1.Rows.Count > 0)
-                        dataGridView1.Rows[0].Selected = true;
+                    foreach (DataRow row in table.Rows)
+                    {
+                        // 2. KREIRANJE GLAVNE KARTICE (PANEL)
+                        Panel card = new Panel();
+                        // Umjesto: card.Size = new Size(flowHistory.Width - 25, 90);
+                        // Stavi fiksno:
+                        card.Size = new Size(350, 90); // Širina prilagođena sa malim paddingom za scrollbar
+                        card.BackColor = cardColor;
+                        card.Margin = new Padding(10, 5, 10, 5);
+                        card.Padding = new Padding(10);
+
+                        // 3. IZRAZ (Gornji tekst - sivi)
+                        Label lblIzraz = new Label();
+                        lblIzraz.Text = row["Izraz"].ToString();
+                        lblIzraz.ForeColor = Color.DarkGray;
+                        lblIzraz.Font = new Font("Segoe UI", 10, FontStyle.Regular);
+                        lblIzraz.Dock = DockStyle.Top;
+                        lblIzraz.AutoSize = true;
+
+                        // 4. REZULTAT (Srednji tekst - zeleni/veliki)
+                        Label lblRezultat = new Label();
+                        lblRezultat.Text = "= " + row["Rezultat"].ToString();
+                        lblRezultat.ForeColor = resultColor;
+                        lblRezultat.Font = new Font("Segoe UI", 14, FontStyle.Bold);
+                        lblRezultat.Location = new Point(10, 30);
+                        lblRezultat.AutoSize = true;
+
+                        // 5. DATUM (Mali tekst u uglu)
+                        Label lblDatum = new Label();
+                        DateTime dt = Convert.ToDateTime(row["Datum"]);
+                        lblDatum.Text = dt.ToString("dd.MM.yyyy HH:mm");
+                        lblDatum.ForeColor = Color.DimGray;
+                        lblDatum.Font = new Font("Segoe UI", 8);
+                        lblDatum.Dock = DockStyle.Bottom;
+                        lblDatum.TextAlign = ContentAlignment.BottomRight;
+
+                        // 6. DODAVANJE ELEMENATA NA KARTICU
+                        card.Controls.Add(lblRezultat);
+                        card.Controls.Add(lblIzraz);
+                        card.Controls.Add(lblDatum);
+
+                        // 7. HOVER EFEKAT (Interaktivnost)
+                        card.MouseEnter += (s, e) => card.BackColor = Color.FromArgb(55, 55, 60);
+                        card.MouseLeave += (s, e) => card.BackColor = cardColor;
+
+                        // Dodaj karticu u flowHistory panel
+                        flowHistory.Controls.Add(card);
+                    }
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Greška prilikom učitavanja istorije: " + ex.Message, "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Greška pri učitavanju: " + ex.Message);
             }
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        // Ako želiš da se lista osvježi kad god se prozor aktivira
+        private void HistoryForm_Activated(object sender, EventArgs e)
+        {
+            // Opcionalno: UcitajIstorijuUKartice();
+        }
+
+        private void HistoryForm_Load_1(object sender, EventArgs e)
         {
 
         }
--- Kalkulator/MainForm.Designer.cs
+++ Kalkulator/MainForm.Designer.cs
@@ -35,16 +35,21 @@
             this.btnHistory = new System.Windows.Forms.Button();
             this.btnCurrency = new System.Windows.Forms.Button();
             this.btnExitApplication = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblTitle
             // 
             this.lblTitle.AutoSize = true;
             this.lblTitle.BackColor = System.Drawing.Color.Transparent;
-            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Bold);
-            this.lblTitle.Location = new System.Drawing.Point(215, 9);
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(264, 9);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(338, 54);
+            this.lblTitle.Size = new System.Drawing.Size(297, 47);
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "Smart Calculator";
             this.lblTitle.Click += new System.EventHandler(this.label1_Click);
@@ -57,9 +62,9 @@
             this.btnScale.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnScale.FlatAppearance.BorderSize = 0;
             this.btnScale.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnScale.Location = new System.Drawing.Point(150, 268);
+            this.btnScale.Location = new System.Drawing.Point(218, 239);
             this.btnScale.Name = "btnScale";
-            this.btnScale.Size = new System.Drawing.Size(162, 146);
+            this.btnScale.Size = new System.Drawing.Size(151, 146);
             this.btnScale.TabIndex = 1;
             this.btnScale.UseVisualStyleBackColor = false;
             this.btnScale.Click += new System.EventHandler(this.btnScale_Click);
@@ -72,9 +77,9 @@
             this.btnKalkulator.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnKalkulator.FlatAppearance.BorderSize = 0;
             this.btnKalkulator.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnKalkulator.Location = new System.Drawing.Point(108, 93);
+            this.btnKalkulator.Location = new System.Drawing.Point(30, 81);
             this.btnKalkulator.Name = "btnKalkulator";
-            this.btnKalkulator.Size = new System.Drawing.Size(204, 146);
+            this.btnKalkulator.Size = new System.Drawing.Size(170, 146);
             this.btnKalkulator.TabIndex = 2;
             this.btnKalkulator.UseVisualStyleBackColor = false;
             this.btnKalkulator.Click += new System.EventHandler(this.button2_Click);
@@ -87,9 +92,9 @@
             this.btnHistory.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnHistory.FlatAppearance.BorderSize = 0;
             this.btnHistory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnHistory.Location = new System.Drawing.Point(448, 268);
+            this.btnHistory.Location = new System.Drawing.Point(538, 239);
             this.btnHistory.Name = "btnHistory";
-            this.btnHistory.Size = new System.Drawing.Size(154, 146);
+            this.btnHistory.Size = new System.Drawing.Size(146, 146);
             this.btnHistory.TabIndex = 3;
             this.btnHistory.UseVisualStyleBackColor = false;
             this.btnHistory.Click += new System.EventHandler(this.btnHistory_Click);
@@ -102,7 +107,7 @@
             this.btnCurrency.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnCurrency.FlatAppearance.BorderSize = 0;
             this.btnCurrency.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCurrency.Location = new System.Drawing.Point(448, 93);
+            this.btnCurrency.Location = new System.Drawing.Point(359, 81);
             this.btnCurrency.Name = "btnCurrency";
             this.btnCurrency.Size = new System.Drawing.Size(154, 146);
             this.btnCurrency.TabIndex = 4;
@@ -117,18 +122,73 @@
             this.btnExitApplication.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnExitApplication.FlatAppearance.BorderSize = 0;
             this.btnExitApplication.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnExitApplication.Location = new System.Drawing.Point(685, 328);
+            this.btnExitApplication.Location = new System.Drawing.Point(685, 12);
             this.btnExitApplication.Name = "btnExitApplication";
             this.btnExitApplication.Size = new System.Drawing.Size(103, 110);
             this.btnExitApplication.TabIndex = 5;
             this.btnExitApplication.UseVisualStyleBackColor = false;
             this.btnExitApplication.Click += new System.EventHandler(this.btnExitApplication_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(71, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Kalkulator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(580, 388);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 21);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Istorija";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(408, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 21);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Valute";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(254, 388);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 21);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Konverter";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(714, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 21);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Izlaz";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnExitApplication);
             this.Controls.Add(this.btnCurrency);
             this.Controls.Add(this.lblTitle);
@@ -151,5 +211,10 @@
         private System.Windows.Forms.Button btnHistory;
         private System.Windows.Forms.Button btnCurrency;
         private System.Windows.Forms.Button btnExitApplication;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
